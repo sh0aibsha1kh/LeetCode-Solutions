1f7d8ac9e23791f027e4f5fa26949cb14016a092
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-02-26 02:29:30 -0500

    Solve 3. Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters.py
+++ 3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters.py
@@ -23,9 +23,12 @@ class Solution:
     answer must be a substring, "pwke" is a subsequence and not a substring.
     """
 
-    def longest_substring_without_repeating_characters(self, s):
+    def naive_longest_substring_without_repeating_characters(self, s):
         """
-
+        Loop through the string and for each index, run through the string once
+        more starting at that index in order to keep track of the longest
+        substring length.
+        Time Complexity: O(n^2)
         """
         seen = set()
         longest_substring_length = 0
@@ -44,9 +47,30 @@ class Solution:
             i += 1
         return longest_substring_length
 
+    def longest_substring_without_repeating_characters(self, s):
+        """
+        Loop through the string once and keep track of the longest substring
+        without repeating characters as well as its starting index. For each
+        iteration, if the character has been seen AND the starting index is
+        less than or equal to the index of the current character, move the
+        starting index of our substring over by one.
+        Time Complexity: O(n)
+        """
+        longest = 0
+        starting_index = 0
+        seen = dict()
+        for i in range(len(s)):
+            if s[i] in seen and starting_index <= seen.get(s[i]):
+                starting_index = seen.get(s[i]) + 1
+            else:
+                longest = max(longest, i - starting_index + 1)
+            seen[s[i]] = i
+        return longest
+
 
 if __name__ == '__main__':
     s = Solution()
     print(s.longest_substring_without_repeating_characters("abcabcbb"))  # -> 3
     print(s.longest_substring_without_repeating_characters("bbbbb"))  # -> 1
     print(s.longest_substring_without_repeating_characters("pwwkew"))  # -> 3
+    print(s.longest_substring_without_repeating_characters("dvdf"))  # -> 3
