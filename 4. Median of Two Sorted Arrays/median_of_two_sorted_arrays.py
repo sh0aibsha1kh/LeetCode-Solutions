155073573e2898b106b47fec0aa007960a81d978
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-03-02 20:33:51 -0500

    Solve 4. Median of Two Sorted Arrays

--- 4. Median of Two Sorted Arrays/median_of_two_sorted_arrays.py
+++ 4. Median of Two Sorted Arrays/median_of_two_sorted_arrays.py
@@ -55,8 +55,67 @@ class Solution:
         else:
             return merged[len(merged) // 2]
 
+    def median_of_two_sorted_arrays(self, nums1, nums2):
+        """
+        The idea here is to partition both arrays in such a way that the number
+        of elements on the left side and right side are both equal. Since we
+        know that the left and right partitions are of equal sizes, then if
+        the maximum element on the left is less than the minimum element on the
+        right for both partitioned arrays, we have found the median.
+        Time Complexity: O(log(min(n,m)))
+        """
+        if len(nums1) > len(nums2):
+            return self.median_of_two_sorted_arrays(nums2, nums1)
+
+        MAX_VALUE = 999999999
+        MIN_VALUE = -999999999
+
+        x = len(nums1)
+        y = len(nums2)
+        is_even = True if (x+y) % 2 == 0 else False
+        i_min = 0
+        i_max = x
+
+        if is_even:
+            if x == 0:
+                return (nums2[y // 2] + nums2[(y // 2) - 1]) / 2
+            if y == 0:
+                return (nums1[x // 2] + nums1[(x // 2) - 1]) / 2
+        else:
+            if x == 0:
+                return nums2[y // 2]
+            if y == 0:
+                return nums1[x // 2]
+
+        while i_min <= i_max:
+            x_partition = (i_min + i_max) // 2
+            y_partition = (x + y + 1) // 2 - x_partition
+
+            x_left = MIN_VALUE if x_partition == 0 else nums1[x_partition - 1]
+            x_right = MAX_VALUE if x_partition == x else nums1[x_partition]
+
+            y_left = MIN_VALUE if y_partition == 0 else nums2[y_partition - 1]
+            y_right = MAX_VALUE if y_partition == y else nums2[y_partition]
+
+            if x_left <= y_right and y_left <= x_right:
+                if is_even:
+                    return (max(x_left, y_left) + min(x_right, y_right)) / 2
+                else:
+                    return max(x_left, y_left)
+
+            elif x_left > y_right:
+                i_max = x_partition - 1
+
+            else:
+                i_min = x_partition + 1
+
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.naive_median_of_two_sorted_arrays([1, 3], [2]))  # -> 2.0
-    print(s.naive_median_of_two_sorted_arrays([1, 2], [3, 4]))  # -> 2.5
+    print(s.median_of_two_sorted_arrays([1, 3], [2]))  # -> 2
+    print(s.median_of_two_sorted_arrays([1, 2], [3, 4]))  # -> 2.5
+    print(s.median_of_two_sorted_arrays([2], []))  # -> 2
+    print(s.median_of_two_sorted_arrays([1, 2, 3, 5, 6], [4]))  # -> 3.5
+    print(s.median_of_two_sorted_arrays([3, 4, 5, 6], [1, 2]))  # -> 3.5
+    print(s.median_of_two_sorted_arrays([1, 2], [3, 4]))  # -> 2.5
+    print(s.median_of_two_sorted_arrays([], [2, 3]))  # -> 2.5
