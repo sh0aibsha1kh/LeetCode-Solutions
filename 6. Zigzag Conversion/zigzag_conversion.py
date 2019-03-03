77080c095ae48afb528843dea02b70b8efcfc659
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-03-03 13:02:26 -0500

    Implement naive solution

--- 6. Zigzag Conversion/zigzag_conversion.py
+++ 6. Zigzag Conversion/zigzag_conversion.py
@@ -23,5 +23,42 @@ class Solution:
     Y A   H R
     P     I
     """
+
     def naive_zigzag_conversion(self, s, num_rows):
-        pass
+        """
+        Create an empty s x n matrix. Start by adding the characters to the
+        matrix going down. Once you hit the last row, zigzag back up. At the
+        end, go through the matrix once more and concatenate the zigzagged
+        values to a new string and return it.
+        Time Complexity: O(s*n)
+        """
+        matrix = [['0' for i in range(len(s))] for j in range(num_rows)]
+        i = 0
+        j = 0
+        zigzag_start = False
+        for char in s:
+            if zigzag_start:
+                i -= 1
+                j += 1
+                matrix[i][j] = char
+            if i == 0:
+                zigzag_start = False
+            if not zigzag_start:
+                matrix[i][j] = char
+                i += 1
+            if i == num_rows:
+                i -= 1
+                zigzag_start = True
+
+        zigzag = ""
+        for row in matrix:
+            for col in row:
+                if col != "0":
+                    zigzag += col
+        return zigzag
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.naive_zigzag_conversion("PAYPALISHIRING", 3))  # -> "PAHNAPLSIIGYIR"
+    print(s.naive_zigzag_conversion("PAYPALISHIRING", 4))  # -> "PINALSIGYAHRPI"
