ad072ffa5d32dcfd11bff8e5c1b51277bfd5823f
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-03-08 00:08:53 -0500

    Implement naive solution

---
+++ 7. Reverse Integer/reverse_integer.py
@@ -0,0 +1,70 @@
+class Solution:
+    """
+    Given a 32-bit signed integer, reverse digits of an integer.
+
+    Example 1:
+
+    Input: 123
+    Output: 321
+
+    Example 2:
+
+    Input: -123
+    Output: -321
+
+    Example 3:
+
+    Input: 120
+    Output: 21
+
+    Note:
+    Assume we are dealing with an environment which could only store integers 
+    within the 32-but signed integer range: [-231, 231 - 1]. For the purpose of
+    this problem, assume that your function returns 0 when the reversed integer
+    overflows.
+    """
+
+    def naive_reverse_integer(self, x):
+        """
+        Create a boolean to check if the value is negative. Based on that,
+        convert the integer into a digit array including/excluding the '-' if
+        it's negative. After reversing the digit array, ignore all of the
+        leading zeroes. Finally, if the reversed number is out of bounds,
+        return 0. Otherwise, return the reversed number.
+        Time Complexity: O(n)
+        """
+
+        is_negative = True if x < 0 else False
+
+        if (is_negative and x > -10) or (not is_negative and x / 10 == 0):
+            return x
+
+        if is_negative:
+            digit_list = [i for i in str(x)[1:]]
+        else:
+            digit_list = [i for i in str(x)]
+        
+        digit_list = digit_list[::-1]
+
+        i = 0
+        while digit_list[i] == '0' and i < len(digit_list):
+            i += 1
+
+        if is_negative:
+            reversed_integer = int("-" + ''.join(digit_list[i::]))
+        else:
+            reversed_integer = int(''.join(digit_list[i::]))
+
+        if reversed_integer < -2**31 or reversed_integer > 2**31 - 1:
+            return 0
+        return reversed_integer
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.naive_reverse_integer(123))  # -> 321
+    print(s.naive_reverse_integer(-123))  # -> -321
+    print(s.naive_reverse_integer(120))  # -> 21
+    print(s.naive_reverse_integer(0))  # -> 0
+    print(s.naive_reverse_integer(-10))  # -> -1
+    print(s.naive_reverse_integer(1534236469))  # -> 0
