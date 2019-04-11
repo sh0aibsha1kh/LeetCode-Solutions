8e2fb30282d94035defe575807bab8a550033900
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-04-11 08:25:33 -0500

    Fix styling

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -3,26 +3,26 @@ import java.util.HashMap;
 
 class Solution {
     /**
-     * Loop through the array once while storing the values and indices inside
-     * a dictionary to help retrieve the complementary index.
+     * Loop through the array once while storing the values and indices inside a
+     * dictionary to help retrieve the complementary index.
      * 
      * Time complexity: O(n)
      */
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             if (map.get(target - nums[i]) == null) {
                 map.put(nums[i], i);
             } else {
-                return new int[]{i, map.get(target-nums[i])};
+                return new int[] { i, map.get(target - nums[i]) };
             }
         }
-        return new int[]{-1, -1};
+        return new int[] { -1, -1 };
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] testNumbers = new int[]{1, 3, 5, 7};
+        int[] testNumbers = new int[] { 1, 3, 5, 7 };
         int[] results1 = s.twoSum(testNumbers, 8);
         int[] results2 = s.twoSum(testNumbers, 10);
         int[] results3 = s.twoSum(testNumbers, 2);
