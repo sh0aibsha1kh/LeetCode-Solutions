311b90fa03f608f4250ef018e2df3c5edc3c07be
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-04-15 16:26:50 -0400

    Solve 21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * Go through both linked lists at the same time recursivel and append the
+     * correct node to a new linked list.
+     * 
+     * Time complexity: O(n)
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
