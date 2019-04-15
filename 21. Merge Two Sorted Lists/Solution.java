dfd1f9e0f748df7c6346beaefcf9fc1af6b2a3b3
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-04-15 18:08:49 -0400

    Implement iteratively instead of recursively

--- 21. Merge Two Sorted Lists/Solution.java
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -1,21 +1,35 @@
 class Solution {
     /**
-     * Go through both linked lists at the same time recursivel and append the
-     * correct node to a new linked list.
+     * Go through both linked lists at the same time and append the correct node to
+     * a new linked list. Use a dummy head and return head.next at the end.
      * 
      * Time complexity: O(n)
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+        ListNode head = new ListNode(0);
+        ListNode l3 = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                l3.next = l1;
+                l1 = l1.next;
+            } else {
+                l3.next = l2;
+                l2 = l2.next;
+            }
+            l3 = l3.next;
         }
+
+        while (l1 != null) {
+            l3.next = l1;
+            l1 = l1.next;
+            l3 = l3.next;
+        }
+
+        while (l2 != null) {
+            l3.next = l2;
+            l2 = l2.next;
+            l3 = l3.next;
+        }
+        return head.next;
     }
 }
